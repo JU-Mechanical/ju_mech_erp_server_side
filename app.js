5b306e3cb1003f9bbcffa5d9305078b5f31f4911
D-Jester <pingking29705@gmail.com>
2025-05-06 18:33:15 +0530

    minor adjustment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI =
+const MONGO_URI = 
   process.env.MONGO_URI || "mongodb://localhost:27017/ju_mech_erp";
 app.use(cors());
 // Middleware
