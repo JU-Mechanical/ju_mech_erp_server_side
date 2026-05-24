876ed037ca619248e44698fffaf932ec227552b7
RSWDevian <pingking29705@gmail.com>
2026-05-24 23:21:57 +0530

    cors2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import { sendIncompleteUsersReminder } from "./controllers/adminController.js";
 dotenv.config();
 
 const corsOptions = {
-  origin: "https://www.jumechanical.online/",
+  origin: "https://www.jumechanical.online",
   opttionSuccessStatus: 200,
 }
 
